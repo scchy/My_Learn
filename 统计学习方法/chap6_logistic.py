ad462774514c474d1ae57d1a6d4cd07a5354ad72
Scc_hy <34824890+scchy@users.noreply.github.com>
2019-12-13 20:13:15 +0800

    Update and rename chap6_logistic to chap6_logistic.py
    增加sklearn实例

--- 统计学习方法/chap6_logistic.py
+++ 统计学习方法/chap6_logistic.py
@@ -11,16 +11,9 @@ from time import clock
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
-from scipy.optimize import leastsq
 from sklearn.datasets import load_iris
 import seaborn as sns
-from itertools import combinations
 from sklearn.model_selection import train_test_split
-from collections import Counter # 将列表都是数，读取成字典
-import pprint
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import export_graphviz
 import pydotplus
 
 def create_data(return_numpy=True):
@@ -46,7 +39,8 @@ def create_data(return_numpy=True):
 ## -------------------------------------
 
 x, y = create_data(True)
-x_tr, x_te, y_tr, y_te = train_test_split(x, y, test_size = 0.3)
+x_tr, x_te, y_tr, y_te = train_test_split(x[:,[0,1]], y, test_size = 0.3)
+
 
 class Lg_reg_clf():
     def __init__(self, max_iter = 200, lr = 0.01):
@@ -87,3 +81,39 @@ class Lg_reg_clf():
 lr_clf = Lg_reg_clf()
 lr_clf.fit(x_tr, y_tr)
 lr_clf.score(x_te, y_te)
+
+
+x_points = np.arange(4, 8)
+x_points2 = -(lr_clf.weights[1] * x_points + lr_clf.weights[0])/ lr_clf.weights[2]
+
+plt.plot(x_points, x_points2)
+plt.scatter(x[:50, 0], x[:50, 1], label='0')
+plt.scatter(x[50:, 0], x[50:, 1], label='1')
+plt.legend()
+plt.show()
+
+## 6.2 sklearn 实例
+## -------------------------------------
+"""
+solver参数决定了我们对逻辑回归损失函数的优化方法，有四种算法可以选择，分别是：
+a) liblinear：使用了开源的liblinear库实现，内部使用了坐标轴下降法来迭代优化损失函数。
+b) lbfgs：拟牛顿法的一种，利用损失函数二阶导数矩阵即海森矩阵来迭代优化损失函数。
+c) newton-cg：也是牛顿法家族的一种，利用损失函数二阶导数矩阵即海森矩阵来迭代优化损失函数。
+d) sag：即随机平均梯度下降，是梯度下降法的变种，和普通梯度下降法的区别是每次迭代仅仅用一部分的样本来计算梯度，适合于样本数据多的时候。
+"""
+from sklearn.linear_model import LogisticRegression
+clf = LogisticRegression(max_iter=200)
+clf.fit(x_tr, y_tr)
+clf.score(x_te, y_te)
+
+x_ponits = np.arange(4, 8)
+y_ = -(clf.coef_[0][0]*x_ponits + clf.intercept_)/clf.coef_[0][1]
+plt.plot(x_ponits, y_)
+
+plt.plot(x[:50, 0], x[:50, 1], 'bo', color='blue', label='0')
+plt.plot(x[50:, 0], x[50:, 1], 'bo', color='orange', label='1')
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend()
+plt.show()
+
