a3ed5678b4744a52c1fa96e6f05deca8d5646c92
Scc_hy <34824890+scchy@users.noreply.github.com>
2020-05-06 11:50:14 +0800

    到 Broadcasting

--- tf2/learn_tf2_基础(2).py
+++ tf2/learn_tf2_基础(2).py
@@ -10,7 +10,7 @@ import numpy as np
 # ======== 目录 ==========
 # &4 tf基础
 #   - 4.7 维度变换
-
+#   - 4.8 Broadcasting
 # ========================
 
 # =======================================================================================================
@@ -37,3 +37,52 @@ x = tf.reshape(x, [2, -1, 3])
 x.ndim, x.shape
 
 ### 4.7.2 增删维度
+x = tf.random.uniform([28, 28], maxval=10, dtype=tf.int32)
+x.shape
+## 1) 增加维度
+x1 = tf.expand_dims(x, axis=2)
+x1.shape
+x2 = tf.expand_dims(x, axis=0)
+x2.shape
+
+## 2) 删除维度
+x21 = tf.squeeze(x2, axis=0)
+## 不指定维度就是将删除所有维度是1的维度
+x = tf.random.uniform([1, 28, 28, 1], maxval=10, dtype=tf.int32)
+xs = tf.squeeze(x)
+"""
+>>> x.shape, xs.shape
+(TensorShape([1, 28, 28, 1]), TensorShape([28, 28]))
+"""
+
+### 4.7.3 交换维度
+x = tf.random.normal([2, 32, 32, 3])
+x1 = tf.transpose(x, perm=[0,3,1,2])
+x.shape, x1.shape
+
+### 4.7.4 数据复制
+"""
+考虑Y = X@W + b的例子，偏置b插入新维度后， 需要在新维度上复制bacth size份， 将shape变为与 X@W 一致后，才能完成相加
+
+以输入[2, 4]为例，输出为三个节点线性变换层为例，偏置b定义为
+b = [1, 2, 3]
+"""
+b = tf.constant([1, 2, 3])
+b = tf.expand_dims(b, axis=0)
+B = tf.tile(b, multiples=[2, 1]) # axis=0复制一次
+B
+
+x = tf.range(4)
+x = tf.reshape(x, [2,2])
+tf.tile(x, multiples=[1,3]) # axis=1 复制2次
+"""
+>>> tf.tile(x, multiples=[1,3])
+<tf.Tensor: id=68, shape=(2, 6), dtype=int32, numpy=
+array([[0, 1, 0, 1, 0, 1],
+       [2, 3, 2, 3, 2, 3]])>
+"""
+
+## 4.8 Broadcasting
+#-----------------------------------------------
+
+
