03d960d1f076af184ddd6b45b51bfaf75573e603
Scc_hy <34824890+scchy@users.noreply.github.com>
2020-05-18 14:27:48 +0800

    Update chap7_svm.py

--- 统计学习方法/chap7_svm.py
+++ 统计学习方法/chap7_svm.py
@@ -1,1 +1,44 @@
-scv
+# python 3.6
+# Author:              Scc_hy
+# Create date:         2020-05-18
+# Function:            统计学习方法
+# Version :
+
+
+import sys, os
+from random import random
+from time import clock
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+import pydotplus
+
+def create_data(return_numpy=True):
+    """
+    载入iris的前一百的数据
+    """
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['label'] = iris.target
+    df.columns = [i.replace(' (cm)', '') for i in df.columns]
+    if return_numpy:
+        data = np.array(df.iloc[:100, :])
+        out_1, out_2 = data[:, :-1], data[:, -1]
+    else:
+        out_1, out_2 = df.iloc[:100, :-1], df.iloc[:100, -1]
+    return out_1, out_2
+
+# ================================================================
+#                   第七章   SVM
+# ================================================================
+## 7.1 
+## -------------------------------------
+
+
+
+
+
+
