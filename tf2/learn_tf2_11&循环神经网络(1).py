459c686f2b250145290d8a0ce3ad54c4165454a5
Scc_hy <34824890+scchy@users.noreply.github.com>
2020-06-16 23:10:17 +0800

    Update learn_tf2_11&循环神经网络(1).py
    # &11 循环神经网络
    #   - 11.1 序列表示方法
    #   - 11.2 循环神经网络
    #   - 11.3 梯度传播
    #   - 11.4 RNN层使用方法

--- tf2/learn_tf2_11&循环神经网络(1).py
+++ tf2/learn_tf2_11&循环神经网络(1).py
@@ -11,6 +11,8 @@ import numpy as np
 # &11 循环神经网络
 #   - 11.1 序列表示方法
 #   - 11.2 循环神经网络
+#   - 11.3 梯度传播
+#   - 11.4 RNN层使用方法
 # ========================
 
 # =======================================================================================================
@@ -70,7 +72,101 @@ Embedding层的查询表是随机初始化的，需要从空开始训练。实�
 embed_glove = load_embed('glovr.6B.59d.txt')
 # 直接利用预训练的词向量初始化Embedding层
 
+
 # 11.2 循环神经网络
 # ---------------------------------------------------
+"""
+考虑一个句子
+I hate this boring movie.
+
+可以转换为[b, s, f], s为句子长度，f为词向量长度。
+[1, 5, 10]
+
+"""
+## 11.2.1 全连接层可行吗
+"""
+- 网络参数态多
+- 每个全连接层子网络Wi, bi只能感受当前某个单词向量的输入，并不能感知前面的单词语境
+和后面单词的语境信息，导致句子整体语义的丢失，每个子网络只能根据自己的输入来提取高层特征
+有如管中窥豹
+
+"""
+## 11.2.2 权值共享
+"""
+想当于使用一个全连接网络来提取所有单词的特征信息。
+但任然是将句子拆开来分布理解，无法获取整体的语义信息。
+"""
+## 11.2.3 语义信息
+"""
+状态张量h，
+ht = sigma(Wxh*xt + Whh * h(t-1) + b)
+h0 初始化为0
+"""
+## 11.2.4 循环神经网络
+"""
+在每个时间戳， 网络层接受当前时间戳的输入xt 和上一个时间戳的网络状态向量h(t-1)
+ht = f_theata(h_t-1, x_t)
+
+ht = sigma(Wxh*xt + Whh * h(t-1) + b)
+激活函数更多采用tanh函数，可以选择不使用偏置bias来进一步减少参数量。状态向量ht可以直接用作输出，
+即ot = ht，也可以对ht做个线性变换ot = w_ho*h_t 得到每个时间戳上的网络输出ot
+"""
+
+# 11.3 梯度传播
+# ---------------------------------------------------
+"""
+d(l)/d(whh) = d(l)/d(ot) * d(ot)/d(ht) * d(ht)/d(hi) * d(hi)/d(whh)
+其中 d(l)/d(ot) 可以基于算是函数直接求出， ot = ht的情况下：
+d(ot)/d(ht) = 1
+
+d(hi)/d(whh) = d(sigma(Wxh*xt + Whh * h(t-1) + b))/d(whh)
+i = 1..t
+
+d(ht)/d(hi) = 连乘 d(hk+1)/d(hk)
+
+d(hk)/d(hi) 包含了连乘运算，这是导致循环神经网络训练困难的根本原因
+"""
+
+# 11.4 RNN层使用方法
+# ---------------------------------------------------
+"""
+layers.SimpleRNNCell 可以完成 sigma(Wxh*xt + Whh * h(t-1) + b)
+Cell仅仅完成一个时间戳的向前运算，不带Cell的层一般是基于Cell实现的，在内部已经完成了
+多个时间戳的循环运算，因此使用起来更为方便快捷。
+"""
+## 11.4.1 SimpleRNNCell
+"""
+以输入特征长度f = 4，Cell状态向量特征长度为h=3为例，首先我们新建一个SimpleRNNCell，不需要指定长度s
+"""
+from tensorflow.keras import layers
+import tensorflow as tf
+cell = layers.SimpleRNNCell(3)
+cell.build(input_shape=(None, 4))
+cell.trainable_variables
+# kernel 变量Wxh张量， recurrent_kernel Whh张量
+"""
+通过一个List包裹起来，这么写是为了与LSTM, GRU等RNN变种格式统一
+ot, [ht] = Cell(xt, [ht-1])
+
+"""
+
+h0 = [tf.zeros([4, 64])]
+# b s f
+x = tf.random.normal([4, 80, 100])
+xt = x[:, 0, :]
+# 构建输入特征f=100, 序列长度s=80, 状态长度为64的Cell
+cell = layers.SimpleRNNCell(64)
+out, h1 = cell(xt, h0)
+print(out.shape, h1[0].shape)
+# 在序列长度的维度解开输入，得到xt:[b f]
+for  xt in tf.unstack(x, axis=1):
+    out, h = cell(xt, h) # 前向计算
+
+out
+
+# 11.4.2 多层SimpleRNNCell网络
+
+
+
 
 
