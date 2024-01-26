be0f301bc65fbea0a6e5b7d07327bfaf15e13191
Scc_hy <296294812@qq.com>
2024-01-26 17:28:55 +0800

    [ LLM-DL ]- :memo: Doc: add README

--- LLM_prime/xtuner_download/test_download.py
+++ LLM_prime/xtuner_download/test_download.py
@@ -1,8 +1,7 @@
-
-
 from download_model import xtunerModelDownload
 from download_dataset import xtunerDataDownload
 from tqdm.auto import tqdm
+import time 
 
 def main():
     print('>>>>>>>> Start xtunerModelDownload')
@@ -18,13 +17,15 @@ def main():
     d_data = xtunerDataDownload(
         data_name,
         out_path='/root/tmp/download_data',
-        tqdm_class=tqdm
+        tqdm_class=tqdm,
+        retry_times=0
     )
     d_data.auto_download()
-
+    time.sleep(60)
+    d_data.break_download()
+    d_model.break_download()
+    print('Yes')
+    
 
 if __name__ == '__main__':
     main()
-
-
-
